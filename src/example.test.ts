8c714d22b2e6f45b639219b64857891143d67059
branilaa <stefan.branila55@gmail.com>
2024-01-06 21:13:24 +0100

    Reset readme file, removed semicolons, modified some package.json scripts.

---
+++ src/example.test.ts
@@ -0,0 +1,7 @@
+import { describe, it, expect } from 'vitest'
+
+describe('sum test', () => {
+  it ('should return 3', () => {
+    expect(1 + 2).toBe(3)
+  })
+})
---
+++ src/lib/server/index.ts
@@ -0,0 +1,2 @@
+// place files you want to import through the `$lib/server` alias in this folder. 
+// SvelteKit will prevent them from being bundled in the client build.
