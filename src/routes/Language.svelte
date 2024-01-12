e8d0c7481a6b64646d6230ac624096c9463505ca
branilaa <stefan.branila55@gmail.com>
2024-01-12 11:26:19 +0100

    navbar changes

---
+++ src/routes/Language.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+  function changeLanguage(language: string) {
+    console.log(language)
+  }
+</script>
+
+<div>
+  <button on:click={() => changeLanguage('it')}>
+    IT
+  </button>
+  
+  <button on:click={() => changeLanguage('en')}>
+    EN
+  </button>
+</div>
+
+<style>
+  div {
+    width: fit-content;
+  }
+
+  button {
+    background-color: var(--secondary);
+    color: var(--primary);
+    border: 0;
+  }
+</style>
--- src/routes/Navbar.svelte
+++ src/routes/Navbar.svelte
@@ -1,9 +1,28 @@
+<script>
+  import Language from './Language.svelte'
+</script>
+
 <nav>
-  Tripensiero
+  <a class="logo" href="/">
+    Tripensiero
+  </a>
+
+  <Language/>
 </nav>
 
 <style>
   nav {
     height: 100px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    background-color: var(--secondary);
+    color: var(--primary);
+    padding: 0 clamp(25px, 15vw, 15vw);
+  }
+
+  nav a {
+    text-decoration: none;
+    color: var(--primary);
   }
 </style>
