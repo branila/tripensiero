c78759213e39e39269d26002d3983cc2c76ea2d5
branilaa <stefan.branila55@gmail.com>
2024-07-30 23:25:22 +0200

    modified main layout file

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,16 +5,31 @@
   import Footer from '$components/layout/Footer.svelte'
 </script>
 
-<Navbar/>
 
-<div class="container">
-  <slot/>
+<div class="outer-container">
+  <Navbar/>
+
+  <div class="inner-container">
+    <slot/>
+  </div>
+
+  <Footer/>
 </div>
 
-<Footer/>
 
 <style>
-  .container {
-    min-height: calc(100svh - 100px);
+  .outer-container {
+    padding-inline: 40px;
+    background-color: var(--dark);
+  }
+
+  .inner-container {
+    min-height: calc(100vh - 75px);
+  }
+
+  @media (max-width: 600px) {
+    .outer-container {
+      padding-inline: 20px;
+    }
   }
 </style>
